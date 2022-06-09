661b17a3b4634d131023cba429f6c12392661aed
Roman Radchenko <nodenwwsfww@gmail.com>
2022-06-09 15:28:57 +0300

    Update main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,7 +7,7 @@ import VueMasonry from 'vue-masonry-css';
 import App from './App.vue';
 import router from './router';
 
-//import i18n from '@/i18n';
+import i18n from '@/i18n';
 
 Vue.config.productionTip = false;
 
