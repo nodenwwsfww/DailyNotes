690aab7b9ca750128e3ce5e43cf6de0d6ccbbcd9
Roman Radchenko <nodenwwsfww@gmail.com>
2022-06-09 20:24:38 +0300

    Add types for store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { RootState } from './types'
 
 Vue.use(Vuex)
-const store = new Vuex.Store({
+const store = new Vuex.Store<RootState>({
+    state: {
+        version: '1.0.0'
+    },
     modules: {
     }
 })
