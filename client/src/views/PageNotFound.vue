f27cd6d75aaf7670a789cf1d56e88fb9b5ceee26
Roman Radchenko <nodenwwsfww@gmail.com>
2022-06-10 20:15:40 +0300

    Continue localization

--- client/src/views/PageNotFound.vue
+++ client/src/views/PageNotFound.vue
@@ -3,21 +3,18 @@
     <div class="msg level-item has-text-centered">
       <div>
         <b-icon icon="exclamation" size="is-large" class="huge center-icon" custom-class="huge-icon"></b-icon>
-        <h1 class="title text-center">Page Not Found. Please go back and try again.</h1>
+        <h1 class="title text-center">{{$t('pages.notfound')}}</h1>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { LOCALES, Locales } from "@/i18n/locales";
-import { defaultLocale } from "@/i18n";
 
 export default {
   name: "page-not-found",
   metaInfo: {
     title: '404',
   },
-  data: () => ({ LOCALES, defaultLocale }),
 };
 </script>
