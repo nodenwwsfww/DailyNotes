5d172e1f740f678985e097145f3a4f193e2427cb
Roman Radchenko <nodenwwsfww@gmail.com>
2022-06-10 20:41:09 +0300

    Update Login.vue

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -3,14 +3,10 @@
     <div class="msgs">{{errMsg}}</div>
     <div class="inputs">
         <b-field :type="usernameErr ? 'is-danger' : ''" :message="usernameErr">
-          <i18n path="placeholders.username" tag="b-input">
-            <b-input placeholder="$t('placeholders.username')" size="is-medium" icon="user" v-model="username" @keyup.native.enter="login"></b-input>
-          </i18n>
+          <b-input :placeholder="$t('placeholders.username')" size="is-medium" icon="user" v-model="username" @keyup.native.enter="login"></b-input>
         </b-field>
         <b-field :type="passwordErr ? 'is-danger' : ''" :message="passwordErr">
-          <i18n path="placeholders.password" tag="b-input">
-            <b-input placeholder="$t('placeholders.Password')" type="password" password-reveal size="is-medium" icon="key" v-model="password" @keyup.native.enter="login"></b-input>
-          </i18n>
+          <b-input :placeholder="$t('placeholders.Password')" type="password" password-reveal size="is-medium" icon="key" v-model="password" @keyup.native.enter="login"></b-input>
         </b-field>
         <b-button type="is-primary" size="is-medium" expanded class="mt-20" @click="login" :loading="isLoading">{{ $t('auth.login') }}</b-button>
         <h1 class="mt-20 alt-button" @click="signup" v-if="!hideSignup">{{ $t('auth.signup') }}</h1>
