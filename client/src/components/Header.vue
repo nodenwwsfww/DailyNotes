fc1fae0d87b56b8f5c37fba2c8c3451654883356
Roman Radchenko <nodenwwsfww@gmail.com>
2022-06-17 17:12:20 +0300

    Test z-index

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -7,7 +7,7 @@
             <b-tooltip
               :label="$t('titles.create-note').toString()"
               position="is-bottom"
-              z-index="2"
+              z-index="999"
             >
               <b-icon icon="plus"></b-icon>
             </b-tooltip>
